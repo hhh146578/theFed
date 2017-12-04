85801ccf7036763ca58621216fdc00eed85cdaec
hhh146578 <hhh146578@gmail.com>
2017-12-04 17:34:13 -0500

    icon bounce

--- script/generateMap.js
+++ script/generateMap.js
@@ -1,23 +1,27 @@
 
 var map;
+var newMarker;
 function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, { 
-    zoom: 5,
-    center: new google.maps.LatLng(37.7718079,-122.3980182)
-  });
+	var mapDiv = document.getElementById('map');
+	var map = new google.maps.Map(mapDiv, { 
+		zoom: 5,
+		center: new google.maps.LatLng(37.7718079,-122.3980182)
+	});
 
-  var smallIcon = {
-  	url: "img/icon.jpg", // url
-    scaledSize: new google.maps.Size(50, 50), // scaled size
+	var smallIcon = {
+		url: "img/icon.jpg", // url
+		scaledSize: new google.maps.Size(50, 50), // scaled size
 	}
- 
-  var newMarker = new google.maps.Marker({
-    position: {lat: 37.7718079, lng: -122.3980182},
-    map: map,
-    icon: smallIcon,
-    draggable: true,
-  });
+
+	newMarker = new google.maps.Marker({
+		position: {lat: 37.7718079, lng: -122.3980182},
+		map: map,
+		icon: smallIcon,
+		draggable: true,
+		animation: google.maps.Animation.DROP,
+	});
+	newMarker.addListener('click', toggleBounce);
+	newMarker.setAnimation(google.maps.Animation.BOUNCE);
 
 	google.maps.event.addDomListener(window, "resize", function() {
 		var center = map.getCenter();
@@ -26,3 +30,10 @@ function initMap() {
 	});
 }
 
+function toggleBounce() {
+	if (newMarker.getAnimation() !== null) {
+		newMarker.setAnimation(null);
+	} else {
+		newMarker.setAnimation(google.maps.Animation.BOUNCE);
+	}
+}
